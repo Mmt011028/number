6cc656754078e02b17298d4e00f8c9a160b62a11
Mmt011028 <103395348+Mmt011028@users.noreply.github.com>
2022-04-19 13:45:55 +0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,6 @@
+function myFunction(){
+        var x = document.getElementById("myText").value;
+        var y = document.getElementById("Text").value;
+        var z= Number(x) + Number(y);
+        document.getElementById("demo").innerHTML = z ;
+        }
